e427c3b59fa07f744b5da463efcc69fa4c0ca17e
liamlopes756 <liamlopes2004@gmail.com>
2025-10-22 18:46:12 -0300

    função notificações
    tasks.py criado
    
    scheduler.py criado
    
    implementação com o firebase

--- careon-app/backend/apps/users/urls.py
+++ careon-app/backend/apps/users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import RegisterView, LoginView, UserUpdateView, UserMeView
+from .views import RegisterView, LoginView, UserUpdateView, UserMeView, DeviceTokenView
 from  rest_framework.authtoken.views import obtain_auth_token
 
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path("update/", UserUpdateView.as_view(), name="user-update"),
     path("me/", UserMeView.as_view(), name="user-me"),
+    path('device-token/', DeviceTokenView.as_view(), name='device-token'),
 ]
