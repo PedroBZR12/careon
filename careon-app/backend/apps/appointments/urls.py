b3d1cd31f8fdcd264a5d975128d54608b41ed713
PedroBZR <bzrpedro@gmail.com>
2025-10-27 17:00:27 -0300

    update urls appointments

--- careon-app/backend/apps/appointments/urls.py
+++ careon-app/backend/apps/appointments/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CompromissoListCreateAPIView, CompromissoDetailAPIView
 
 urlpatterns = [
-    path('compromissos/', CompromissoListCreateAPIView.as_view(), name='compromisso-list-create'),
-    path('compromissos/<int:pk>/', CompromissoDetailAPIView.as_view(), name='compromisso-detail'),
+    path('', CompromissoListCreateAPIView.as_view(), name='compromisso-list-create'),
+    path('<int:pk>/', CompromissoDetailAPIView.as_view(), name='compromisso-detail'),
 ]
