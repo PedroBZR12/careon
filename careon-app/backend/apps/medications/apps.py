654d3f238bc3e7c78419792699afa86befd7d743
PedroBZR <bzrpedro@gmail.com>
2025-10-27 16:47:45 -0300

    update apps.py de medications

--- careon-app/backend/apps/medications/apps.py
+++ careon-app/backend/apps/medications/apps.py
@@ -1,3 +1,4 @@
+"""
 from django.apps import AppConfig
 
 
@@ -8,3 +9,30 @@ class MedicationsConfig(AppConfig):
     def ready(self):
         from . import scheduler
         scheduler.start()
+        
+        
+        """
+        
+        
+import os
+import logging
+from django.apps import AppConfig
+
+logger = logging.getLogger(__name__)
+
+class MedicationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.medications"
+
+    def ready(self):
+        enable = os.environ.get("ENABLE_SCHEDULER", "false").lower()
+        if enable != "true":
+            logger.info("Scheduler disabled by ENABLE_SCHEDULER=%s", enable)
+            return
+
+        try:
+            from . import scheduler
+            scheduler.start()
+            logger.info("Scheduler started")
+        except Exception:
+            logger.exception("Failed to start scheduler")
